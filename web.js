a95f0970e1bfb6da224e6715b3a16704c18915df
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2026-07-20 21:38:47 -0400

    further web development

--- web.js
+++ web.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var app = express();
+var path = require("path");
 var port, config;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res)=>{
-    res.send("felper has a website now");
+    res.render('index');
 })
 
 
