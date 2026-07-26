538070e71b6490eb13c847ca7d1742c36e53a19b
Iris <irisuingg@gmail.com>
2026-07-26 12:37:00 -0500

    Moved FunkyHelper says into funkyhelperSays in config (#12)
    Co-authored-by: DraguinGG <mapletim2012@gmail.com>

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res)=>{
-    res.render('index', {active: 'index'});
+    res.render('index', {active: 'index', choices: config["funkyhelperSays"]});
 });
 
 app.get("/privacy", (req, res)=>{
